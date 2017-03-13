dffc83c2f278aeccba1a225d3994424d9df1424c
Kieran Doble <kieran.doble@gmail.com>
2017-03-13 13:59:51 +0000

    Inclusion of all images and initial dev of speed/height

---
+++ carWidth.m
@@ -0,0 +1,6 @@
+function width = carWidth(carDistance, boundingBox)
+    imageWidth = 480; % pixels
+    centerOfImage = (imageWidth / 2);
+    
+    pixelViewAngle = 0.042; % degrees
+    cameraHeight = 7;       % meters
